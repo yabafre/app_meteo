82864482cd1e3d292a457afc80b3ea2308d33832
yabafre <61940604+yabafre@users.noreply.github.com>
2022-04-08 10:27:17 +0200

    final

--- main.js
+++ main.js
@@ -1,64 +1,60 @@
-const apiKey = "c59090be381bac484b2ef64701e2119f";
-
-const nowIcon = document.querySelector(".now-icon");
-const nowDescription = document.querySelector(".description");
-const nowTemperature = document.querySelector(".temperature");
-const hour = document.querySelectorAll(".check");
-const day = document.querySelectorAll(".day");
-const city = document.querySelector("h1");
-let nowDay = new Date().getDay();
-let nowHours = new Date().getHours();
-
-// TODO: Récupérer les éléments  du DOM dans des constantes
-
-// Traiter les erreurs de navigator.geolocation.getCurrentPosition
-const handleGetCurrentPositionError = (error) => {
-  alert("Votre géolocalisation ne fonctionne pas, vérifiez vos paramètres.");
-};
-
-// Requête fetch
+const template = document.querySelector("template");
+const load = document.querySelector(".loading");
+const main = document.querySelector("main");
 
 const getWeatherOf = async (position) => {
   try {
-    // let lat = position.coords.latitude;
-    // let lon = position.coords.longitude;
-
     const { latitude, longitude } = position.coords;
 
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&lang=fr&appid=${apiKey}`
-    );
-    const data = await res.json();
+    const allPromise = Promise.all([
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&lang=fr&appid=1ac3d5e78a389a710094d95fc304e84e`
+      ),
+      fetch(
+        `https://api-adresse.data.gouv.fr/reverse/?lon=${longitude}&lat=${latitude}`
+      ),
+    ]);
+    const [weatherallPromiseult, cityallPromiseult] = await allPromise;
+    const weatherData = await weatherallPromiseult.json();
+    const cityData = await cityallPromiseult.json();
 
-    // console.log(data)
-    updateUI(data);
+    // API Weather
+    updateUI(weatherData);
+    // API Locate
+    updateUI(cityData);
+    // Loading content
+    var clone = document.importNode(template.content, true);
+    main.removeChild(load);
+    setTimeout(5000);
+    main.appendChild(clone);
+    //const Clone
+    const liveIcon = document.querySelector(".live-icon");
+    const liveDescription = document.querySelector(".description");
+    const liveTemp = document.querySelector(".temp");
+    const hour = document.querySelectorAll(".hour");
+    const day = document.querySelectorAll(".day");
+    const city = document.querySelector(".city");
 
-    // NOW
-    city.innerHTML = `${data.timezone}`;
-    nowIcon.src = `img/${data.current.weather[0].icon}.svg`;
-    nowDescription.innerText = `${data.current.weather[0].description}`;
-    nowTemperature.innerText = `${Math.trunc(data.current.temp)}°`;
+    console.log(city);
+    // Live
+    city.innerText = `${cityData.features[0].properties.city}`;
+    liveIcon.src = `img/${weatherData.current.weather[0].icon}.svg`;
+    liveDescription.innerText = `${weatherData.current.weather[0].description}`;
+    liveTemp.innerText = `${Math.trunc(weatherData.current.temp)}°`;
 
-    // HOURS
+    // HOURS : 24h
     for (let i = 0; i < 24; i++) {
-      let j = nowHours + i;
-
-      if (j > 24) {
-        j = j - 24;
-      } else if (j == 24) {
-        j = 0;
-      }
-
-      hour[i].innerHTML = `<p class="hour-text">${j}h</p>
-                                 <img class="hour-icon" src="img/${
-                                   data.hourly[i].weather[0].icon
-                                 }.svg" alt="img temps">
-                                 <p class="hour-temperature">${Math.trunc(
-                                   data.hourly[i].temp
-                                 )}°</p>`;
+      hour[i].innerHTML = `<p class="hour-text">${new Date(
+        weatherData.hourly[i].dt * 1000
+      ).getHours()}h</p>
+      <p class="hour-temp">${Math.trunc(weatherData.hourly[i].temp)}°</p>
+      <img class="hour-icon" src="img/${
+        weatherData.hourly[i].weather[0].icon
+      }.svg" alt="img temps">
+        `;
     }
 
-    // DAYS
+    // DAYS :
     let week = [
       "dimanche",
       "lundi",
@@ -68,37 +64,28 @@ const getWeatherOf = async (position) => {
       "vendredi",
       "samedi",
     ];
-    for (let k = 0; k < 5; k++) {
-      l = nowDay + k;
-      console.log(l);
+    console.log(new Date().getDay());
+    for (let k = 0; k < week.length; k++) {
+      let daily_dt = new Date(weatherData.daily[k].dt * 1000).getDay();
 
-      if (l > 7) {
-        l = l - 7;
-      } else if (l == 7) {
-        l = 0;
-      }
-
-      day[k].innerHTML = `<p class="day-text">${week[l]}</p>
-                                <img class="day-icon" src="img/${
-                                  data.daily[k].weather[0].icon
-                                }.svg" alt="img temps">
-                                <p class="day-temperature">min ${Math.trunc(
-                                  data.daily[k].temp.min
-                                )}°</p>
-                                <p class="day-temperature">max ${Math.trunc(
-                                  data.daily[k].temp.max
-                                )}°</p>`;
+      day[k].innerHTML = `<p class="day-text">${week[daily_dt]}</p>
+        <img class="day-icon" src="img/${
+          weatherData.daily[k].weather[0].icon
+        }.svg" alt="img temps">
+          <p class="day-temp">Min : ${Math.trunc(
+            weatherData.daily[k].temp.min
+          )}°  |  Max : ${Math.trunc(weatherData.daily[k].temp.max)}°</p>`;
     }
   } catch (error) {
     console.error("Erreur dans le getWeatheerOf() ~>", error);
   }
 };
-
 const updateUI = (data) => {
   console.log(data);
+  // const newContent = template.content.cloneNode
 };
 
-// Récupérer la géolocalisation de l'utilisateur et faire la requ^te à l'API
+// Récupérer la géolocalisation de l'utilisateur et faire la requête à l'API
 navigator.geolocation.getCurrentPosition(
   getWeatherOf,
   (error) => console.log("getCurrentPosition error ~>", error),
